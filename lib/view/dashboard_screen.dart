df7e6da185bc3a8c84ce3bc4376b29ea9531b75a
Samuel Santos <mucasantos@gmail.com>
2024-09-18 15:40:21 -0300

    solve scroll problem

--- lib/view/dashboard_screen.dart
+++ lib/view/dashboard_screen.dart
@@ -1,7 +1,7 @@
 import 'package:first_app/constants/images_assets.dart';
 import 'package:first_app/constants/styles.dart';
 import 'package:first_app/mock/produtos_fake_db.dart';
-import 'package:first_app/models/produto_model.dart';
+
 import 'package:first_app/widgets/card_produto.dart';
 import 'package:first_app/widgets/container_propaganda.dart';
 import 'package:flutter/material.dart';
@@ -11,51 +11,52 @@ class Dashboard extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    ProdutoModel produto =
-        ProdutoModel(imagem: AppImages.apple, titulo: "Maça", preco: 6.99);
-    ProdutoModel produto2 = ProdutoModel(
-        imagem: AppImages.refri1, titulo: "Refrigerante", preco: 6.99);
     return SafeArea(
       child: Scaffold(
-        body: Column(
-          children: [
-            Center(
-              child: Image.asset(
-                AppImages.logoCarrot,
-                height: 65,
+        body: SingleChildScrollView(
+          child: Column(
+            children: [
+              Center(
+                child: Image.asset(
+                  AppImages.logoCarrot,
+                  height: 65,
+                ),
               ),
-            ),
-            const Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                Icon(Icons.location_on),
-                Text("Cotia, São Paulo"),
-              ],
-            ),
-            Padding(
-              padding: EdgeInsets.all(32.0),
-              child: TextField(
-                decoration: textFieldDecoration,
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(Icons.location_on),
+                  Text("Cotia, São Paulo"),
+                ],
               ),
-            ),
-            ContainerPropaganda(),
-            Text("OFERTAS"),
-            SizedBox(
-              height: 400,
-              child: GridView.builder(
+              Padding(
+                padding: EdgeInsets.all(32.0),
+                child: TextField(
+                  decoration: textFieldDecoration,
+                ),
+              ),
+              ContainerPropaganda(),
+              Text("OFERTAS"),
+              GridView.builder(
+                shrinkWrap: true,
+                primary: false,
+                physics: NeverScrollableScrollPhysics(),
                 itemCount: appProducts.length, //qtd de produtos
                 gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
                   crossAxisCount: 2,
                   childAspectRatio: 0.8,
                 ),
                 itemBuilder: (context, index) {
-                  return CardProduto(produto: produto);
+                  return CardProduto(produto: appProducts[index]);
                 },
               ),
-            ),
-            Text("Mais vendidos"),
-            Text("Cards de frutas "),
-          ],
+              Text("Mais vendidos"),
+              Text("Cards de frutas "),
+              SizedBox(
+                height: 250,
+              )
+            ],
+          ),
         ),
       ),
     );
